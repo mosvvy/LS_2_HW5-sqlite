ce48304326a8f4beefdd7d39071fe437ccf8b50b
Valeriia Moskalenko <moskalenkovvy@gmail.com>
2024-06-08 16:11:32 +0300

    SQLiteConnector class created. create_table_user() for task pt1 created. Database with table user created

--- sqlite_connector.py
+++ sqlite_connector.py
@@ -1,2 +1,44 @@
 import sqlite3
 
+
+class SQLiteConnector:
+    def __init__(self, path:str):
+        self.__path = path
+        self.__connection = sqlite3.connect(self.__path)
+        self.__cursor = self.__connection.cursor()
+
+    @property
+    def cursor(self):
+        return self.__cursor
+
+    def exec(self, query:str):
+        return  self.__cursor.execute(query)
+
+    def __del__(self):
+        self.__cursor.close()
+        self.__connection.close()
+
+
+def create_table_user():
+    con = SQLiteConnector('auth_db.db')
+
+    con.cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+    id int primary_key,
+    username text unique,
+    password text,
+    email text unique
+    );
+    """)
+
+    del con
+
+
+if __name__ == '__main__':
+    # create_table_user()
+
+    con = SQLiteConnector('auth_db.db')
+
+
+
+    del con
